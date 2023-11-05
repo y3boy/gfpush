1f5fc0a51ed1835ecd9ce99bfa51a8b80543a0d8
y3boy <y3boy@debian12.debian12.vm>
2023-11-05 13:24:50 +0300

    feat: Add type,scope & message flags

--- Makefile
+++ Makefile
@@ -1,3 +1,17 @@
-main.go:
-	echo "Building gfpush ..."
-	go build -o gfpush cli/gfpush/main.go
+all: build check
+
+.PHONY: linter
+linter:
+	golangci-lint run
+
+.PHONY: build
+build:
+	@echo "Building gfpush ..."
+	@ go build -o gfpush cli/gfpush/main.go
+	@echo done ✔️
+.PHONY: check
+check:
+	@echo
+	@echo "Checking gfpush version ..."
+	./gfpush -v
+	@echo done ✔️
--- cli/gfpush/main.go
+++ cli/gfpush/main.go
@@ -2,11 +2,27 @@ package main
 
 import (
 	"os"
-	"fmt"
+	// "fmt"
+	// "strconv"
 	"github.com/urfave/cli/v2"
+	// "github.com/go-git/go-git/v5"
 )
 
 const version = "0.0.1"
+
+func printCommitType() string {
+	return `1: build
+	2: chore
+	3: ci
+	4: docs
+	5: feat
+	6: fix
+	7: perf
+	8: refactor
+	9: revert
+	10: style
+	11: test`
+}
 
 func main() {
 	// commitType := map[int]string{
@@ -22,19 +38,44 @@ func main() {
 	// 	10: "test",
 	// }
 	
+	// setup version flag 
+	cli.VersionFlag = &cli.BoolFlag{
+		Name:    "print-version",
+		Aliases: []string{"v"},
+		Usage:   "print gfpush version",
+	}
+
 	app := &cli.App{
 		Name:  "gfpush",
 		Usage: "commit and push faster",
 		EnableBashCompletion: true,
-		Commands: []*cli.Command{
-			{
-				Name:    "version",
-				Aliases: []string{"v"},
-				Usage:   "print gfpush version",
-				Action: func(cCtx *cli.Context) error {
-					fmt.Println(version)
-					return nil
-				},
+		Authors: []*cli.Author{
+			&cli.Author{
+				Name:  "Anushervon Nabiev",
+				Email: "nabievanush1@gmail.com",
+			},
+		},
+		Version: version,
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:     "all",
+				Aliases:	[]string{"a"},
+				Usage:       "Tell the command to automatically stage files that have been modified and deleted,\n\tbut new files you have not told Git about are not affected.",
+			},
+			&cli.StringFlag{
+				Name:    "message",
+				Aliases: []string{"m"},
+				Usage:   "Use the given `<msg>` as the commit message.",
+			},
+			&cli.StringFlag{
+				Name:    "type",
+				Aliases: []string{"t"},
+				Usage:   ("Type of commit message.\n\t" + printCommitType()),
+			},
+			&cli.StringFlag{
+				Name:    "scope",
+				Aliases: []string{"s"},
+				Usage:   ("Scope of commit."),
 			},
 		},
 	}
